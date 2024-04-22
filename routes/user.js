607bb445d09ca566d87047adb8e722ef3287af64
bhnghiaa <buihainghiaa@gmail.com>
2024-04-23 01:11:52 +0700

    Add JWT token verification to user.js route

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,5 @@ const { verifyToken } = require("../middleware/jwt_token")
 
 router.delete('/', verifyToken, userController.deleteUser)
 router.get('/', verifyToken, userController.getUser)
-router.put('/:id', userController.editUser)
+router.put('/:id', verifyToken, userController.editUser)
 module.exports = router;
