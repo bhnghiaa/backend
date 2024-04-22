ca6e72bc1a37c519677a35b0486f4bfea106ef97
bhnghiaa <buihainghiaa@gmail.com>
2024-04-23 01:07:21 +0700

    Refactor userController.js to include editUser function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,13 @@
 const User = require("../models/User");
 
 module.exports = {
+
+
     deleteUser: async (req, res, next) => {
         try {
             await User.findByIdAndDelete(req.user.id)
 
-            res.status(200).json({status: true, message: "User succesfully deleted"})
+            res.status(200).json({ status: true, message: "User succesfully deleted" })
         } catch (error) {
             return next(error)
         }
@@ -14,15 +16,25 @@ module.exports = {
     getUser: async (req, res, next) => {
         const user_id = req.user.id;
         try {
-            const user = await User.findById({_id: user_id}, {password: 0, __v: 0, createdAt: 0, updatedAt: 0})
+            const user = await User.findById({ _id: user_id }, { password: 0, __v: 0, createdAt: 0, updatedAt: 0 })
 
-            if(!user){
-                return res.status(401).json({status: false, message: "User does not exist"})
+            if (!user) {
+                return res.status(401).json({ status: false, message: "User does not exist" })
             }
 
             res.status(200).json(user)
         } catch (error) {
             return next(error)
         }
-    }
+    },
+
+    editUser: async (req, res, next) => {
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            if (!updatedUser) return next(new Error('User not found'));
+            res.status(200).json(updatedUser);
+        } catch (error) {
+            return next(error);
+        }
+    },
 }
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const userController = require('../controllers/userController')
-const {verifyToken} = require("../middleware/jwt_token")
+const { verifyToken } = require("../middleware/jwt_token")
 
-router.delete('/',verifyToken, userController.deleteUser)
-router.get('/',verifyToken,userController.getUser)
-
+router.delete('/', verifyToken, userController.deleteUser)
+router.get('/', verifyToken, userController.getUser)
+router.put('/:id', userController.editUser)
 module.exports = router;
