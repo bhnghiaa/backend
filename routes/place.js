86858f9b06bac5a1e3d086e24e499d37f94397c0
bhnghiaa <buihainghiaa@gmail.com>
2024-04-22 09:37:09 +0700

    m

--- routes/place.js
+++ routes/place.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const placeController = require('../controllers/placeController');
-const {verifyToken} = require("../middleware/jwt_token")
+const { verifyToken } = require("../middleware/jwt_token")
 
-router.post('/',verifyToken, placeController.addPlaces)
+router.post('/', placeController.addPlaces)
 router.get('/', placeController.getPlaces)
 router.get('/:id', placeController.getPlace)
 router.get('/byCountry/:id', placeController.getPlacesByCountry)
